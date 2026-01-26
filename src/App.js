077cd7d9eca5fea4494e8cc6c2dac14fa23fe414
NikitaChernyavii <nikitakitorage2134@gmail.com>
2026-01-26 18:36:50 +0200

    Second vers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import Items from "./components/Items";
 
 function App() {
+  // Называем переменную items, а не props
+  const [items] = useState([
+    {
+      id: 1,
+      title: 'Hoodie',
+      img: 'HoodieGray.jpg',
+      desc: 'Comfort + Whyte Style.',
+      category: 'Hoodie',
+      price: '49.99'
+    },
+    {
+      id: 2,
+      title: 'Jacket',
+      img: 'jacketWhite.jpg',
+      desc: 'Jacket for StreatWear.',
+      category: 'tables',
+      price: '149.00'
+    },
+    {
+      id: 3,
+      title: 'Hoodie Baggy',
+      img: 'HoodieBlue.jpg',
+      desc: 'Fresh and Ice.',
+      category: 'Hoodie',
+      price: '69.99'
+    }
+  ]);
+
   return (
     <div className="wrapper">
       <Header />
+      {/* Передаем наш массив items */}
+      <Items items={items} />
       <Footer />
-
     </div>
   );
 }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,14 @@ export default function Header() {
     <header>
         <div>
             <span className='logo'>AxN Design</span>
+            <ul className='nav'>
+                <li>About us</li> 
+                <li>Contact</li>
+                <li>Profile</li>
+
+            </ul>
         </div>
+
         <div className='presentation'></div>
     </header>
 )
