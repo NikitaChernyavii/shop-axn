0df3cd191c1eba53360d9456da4af7312e5a0f8b
NikitaChernyavii <nikitakitorage2134@gmail.com>
2026-01-30 17:01:41 +0200

    Full

--- src/App.js
+++ src/App.js
@@ -2,43 +2,58 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
+import Categories from "./components/Categories";
+import ShowFullItem from "./components/ShowFullItem"; // Импортируем новый компонент
 
 function App() {
-  // Называем переменную items, а не props
   const [items] = useState([
-    {
-      id: 1,
-      title: 'Hoodie',
-      img: 'HoodieGray.jpg',
-      desc: 'Comfort + Whyte Style.',
-      category: 'Hoodie',
-      price: '49.99'
-    },
-    {
-      id: 2,
-      title: 'Jacket',
-      img: 'jacketWhite.jpg',
-      desc: 'Jacket for StreatWear.',
-      category: 'tables',
-      price: '149.00'
-    },
-    {
-      id: 3,
-      title: 'Hoodie Baggy',
-      img: 'HoodieBlue.jpg',
-      desc: 'Fresh and Ice.',
-      category: 'Hoodie',
-      price: '69.99'
-    }
+    { id: 1, title: 'Hoodie', img: 'HoodieGray.jpg', desc: 'Comfort + Whyte Style.', category: 'Zip', price: '49.99' },
+    { id: 2, title: 'Jacket', img: 'jacketWhite.jpg', desc: 'Jacket for StreatWear.', category: 'Jacket', price: '149.00' },
+    { id: 3, title: 'Hoodie Baggy', img: 'HoodieBlue.jpg', desc: 'Fresh and Ice.', category: 'Hoodie', price: '69.99' },
   ]);
+
+  const [currentItems, setCurrentItems] = useState(items);
+  const [orders, setOrders] = useState([]);
+  
+  // СОСТОЯНИЕ ДЛЯ МОДАЛЬНОГО ОКНА
+  const [showFullItem, setShowFullItem] = useState(false); // Показываем или нет
+  const [fullItem, setFullItem] = useState({}); // Какой именно товар показываем
+
+  const onShowItem = (item) => {
+    setFullItem(item);
+    setShowFullItem(!showFullItem);
+  }
+
+  const addToOrder = (item) => {
+    let isInArray = orders.some(el => el.id === item.id);
+    if(!isInArray) setOrders([...orders, item]);
+  };
+
+  const deleteOrder = (id) => {
+    setOrders(orders.filter(el => el.id !== id));
+  }
+
+  const chooseCategory = (category) => {
+    if (category === 'all') {
+      setCurrentItems(items);
+      return;
+    }
+    setCurrentItems(items.filter(el => el.category === category));
+  }
 
   return (
     <div className="wrapper">
-      <Header />
-      {/* Передаем наш массив items */}
-      <Items items={items} />
+      <Header orders={orders} onDelete={deleteOrder}/>
+      <Categories chooseCategory={chooseCategory}/>
+      {/* Передаем функцию открытия товара в Items */}
+      <Items items={currentItems} onAdd={addToOrder} onShowItem={onShowItem} />
+
+      {/* УСЛОВИЕ: если showFullItem истина, показываем компонент */}
+      {showFullItem && <ShowFullItem item={fullItem} onAdd={addToOrder} onShowItem={onShowItem} />}
+      
       <Footer />
     </div>
   );
 }
+
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingBag } from 'react-icons/fa'
+import Order from './Order'
 
-export default function Header() {
-  return (
+export default function Header(props) {
+    let [cartOpen, setCartOpen] = useState(false)
+
+    // Вынесем логику показа товаров в функцию, чтобы код был чище
+    const showOrders = (props) => {
+        let summa = 0
+        props.orders.forEach(el => summa += Number.parseFloat(el.price))
+        
+        return (
+            <div>
+                {props.orders.map(el => (
+                    <Order onDelete={props.onDelete} key={el.id} item={el} />
+                
+                ))}
+
+                <p className='summa'>Price: {new Intl.NumberFormat().format(summa)}$</p>
+            </div>
+        )
+    }
+
+    const showNothing = () => {
+        return (
+            <div className='empty'>
+                <h2>Товаров нет</h2>
+            </div>
+        )
+    }
+
+   return (
     <header>
-        <div>
+        {/* Контейнер, который мы будем "флексить" */}
+        <div className='header-top'> 
             <span className='logo'>AxN Design</span>
+            
             <ul className='nav'>
                 <li>About us</li> 
                 <li>Contact</li>
                 <li>Profile</li>
-
+                {/* Корзину кладем внутрь списка, чтобы она была в ряд с текстом */}
+                <li>
+                    <FaShoppingBag 
+                        onClick={() => setCartOpen(!cartOpen)} 
+                        className={`shop-bag-button ${cartOpen ? 'active' : ''}`}
+                    />
+                </li>
             </ul>
+
+            {/* Корзина (выпадающее окно) */}
+            {cartOpen && (
+                <div className='shop-bag'> 
+                    {props.orders.length > 0 ? 
+                        showOrders(props) : showNothing()}
+                </div>
+            )}
         </div>
 
         <div className='presentation'></div>
